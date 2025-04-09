e58f0b65a5cb4af1998c44a936264ccb88cc103f
Bhavya-chowdary <you@example.com>
2025-04-09 12:27:42 +0530

    Exp 14 code commited

---
+++ DeploymentProject/src/main/java/com/klu/HomeController.java
@@ -0,0 +1,15 @@
+package com.klu;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController 
+{
+	@GetMapping({"/", "/home", "/status"})
+	public String getStatus()
+	{
+		return "Application is up and Running..";
+	}
+}
+
